68a6cb98b325483981ba015ac028462570e77c16
CatutoiuAlexandra <81251442+CatutoiuAlexandra@users.noreply.github.com>
2023-12-11 03:12:40 +0200

    Fix merge error

--- Program.cs
+++ Program.cs
@@ -8,32 +8,25 @@ using GameHub.Dto.DtoServices.IDtoServices;
 using GameHub.Dto.DtoServices;
 using GameHub.Utils;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using GameHub.CRUD;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IServiceShoppingCart, ServiceShoppingCart>();
 
-builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-builder.Services.AddScoped<IProductCRUD, ProductCRUD>();
-builder.Services.AddScoped<ICategoryCRUD, CategoryCRUD>();
-builder.Services.AddScoped<IShoppingCartCRUD, ShoppingCartCRUD>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
